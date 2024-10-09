61211524ce5769a76f7a66ead1b0491d1ba2265e
Joaquin H <hernandez.joaquin346@gmail.com>
2024-10-09 09:55:16 -0300

    refactor de metodos

--- src/main/java/org/algo3/modelo/proveedor/ProveedorTxt.java
+++ src/main/java/org/algo3/modelo/proveedor/ProveedorTxt.java
@@ -2,25 +2,30 @@ package org.algo3.modelo.proveedor;
 
 import org.algo3.modelo.Chiste;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Random;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class ProveedorTxt implements Proveedor {
-    private final String rutaArchivo = Paths.get(System.getProperty("user.dir"), "src/chistes/archivo.txt").toString();
+    private final String rutaArchivo = "./src/chistes/chistes.txt";
+
+    private BufferedReader crearBufferedReader() throws FileNotFoundException {
+        return new BufferedReader(new FileReader(this.rutaArchivo));
+    }
 
     public Chiste solicitarChiste(String categoria, String idioma) {
         Random random = new Random();
-        int numero = random.nextInt(6); 
-        if (categoria.equals("Christmas")){
-            numero += 5;
+        int linea = random.nextInt(6);
+        if (categoria.equals("Christmas")) {
+            linea += 5;
         }
         if (idioma.equals("en")) {
-            numero += 10;
+            linea += 10;
         }
-        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+        try (BufferedReader br = crearBufferedReader()) {
             int i = 0;
-            while (i != numero) {
+            while (i != linea) {
                 br.readLine();
                 i += 1;
             }
